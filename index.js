46c16aec18d22acb9f7840b194aa039512491e66
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-10 16:45:30 +0530

    practice-res.json

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/about", (req, res) => {
+  res.json({ name: "user", email: "@email.com" });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
